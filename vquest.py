b7b5200676f7ef1634e6c6922f0bbcf83cfc4f08
Jesse Connell <ancon@upenn.edu>
2020-12-01 14:05:49 -0500

    first draft of chunked requests for AIRR results

--- vquest.py
+++ vquest.py
@@ -7,17 +7,82 @@ http://www.imgt.org/IMGT_vquest/analysis
 """
 
 import sys
-import subprocess
 import argparse
 import logging
+import time
+from zipfile import ZipFile
+from io import StringIO, BytesIO
 from pathlib import Path
 import yaml
 import requests
+from Bio import SeqIO
 
 URL = "http://www.imgt.org/IMGT_vquest/analysis"
+DELAY = 1 # for rate-limiting multiple requests
 LOGGER = logging.getLogger(__name__)
 LOGGER.propagate = False
 LOGGER.addHandler(logging.StreamHandler())
+
+def vquest_main():
+    """Command-line interface for V-QUEST requests"""
+    parser = __setup_arg_parser()
+    args = parser.parse_args()
+    LOGGER.setLevel(max(10, logging.WARNING - 10*args.verbose))
+    LOGGER.debug("args parsed")
+    # If nothing at all was given print the help and exit
+    nonopts = ["config", "verbose"]
+    args_set = {key: val for key, val in vars(args).items() if val}
+    if not args_set:
+        parser.print_help()
+        sys.exit(1)
+    LOGGER.debug("command-line arguments: %s",
+        " ".join(["%s=%s" % (key, val) for key, val in args_set.items()]))
+    # Overlay the default config, configs given as files, and then options
+    # given as command line arguments
+    configs = [load_config(config) for config in args.config]
+    for filename, config in zip(args.config, configs):
+        LOGGER.debug("options via %s: %s",
+            filename,
+            " ".join(["%s=%s" % (key, val) for key, val in config.items()]))
+    vquest_args = {key: val for key, val in args_set.items() if key not in nonopts}
+    LOGGER.debug("overriding command-line options: %s",
+        " ".join(["%s=%s" % (key, val) for key, val in vquest_args.items()]))
+    config_full = layer_configs(DEFAULTS, *configs, vquest_args)
+    LOGGER.debug("final config: %s",
+        " ".join(["%s=%s" % (key, val) for key, val in config_full.items()]))
+    output = vquest(config_full)
+    with open("vquest_airr.tsv", "wt") as f_out:
+        f_out.write(output["vquest_airr.tsv"])
+    with open("Parameters.txt", "wt") as f_out:
+        f_out.write(output["Parameters.txt"])
+
+def vquest(config):
+    """Submit a request to V-QUEST"""
+    supported = [("resultType", "excel"), ("xv_outputtype", 3), ("inputType", "inline")]
+    if all([config.get(pair[0]) == pair[1] for pair in supported]):
+        output = {}
+        with StringIO(config["sequences"]) as seqs_stream:
+            records = list(SeqIO.parse(seqs_stream, "fasta"))
+        for chunk in _chunker(records, 50):
+            if output:
+                time.sleep(DELAY)
+            out_handle = StringIO()
+            SeqIO.write(chunk, out_handle, "fasta")
+            config_chunk = config.copy()
+            config_chunk["sequences"] = out_handle.getvalue()
+            response = requests.post(URL, data = config_chunk)
+            response = _unzip(response.content)
+            if "Parameters.txt" not in output:
+                output["Parameters.txt"] = response["Parameters.txt"].decode()
+            if "vquest_airr.tsv" not in output:
+                output["vquest_airr.tsv"] = response["vquest_airr.tsv"].decode()
+            else:
+                airr = response["vquest_airr.tsv"].decode()
+                output["vquest_airr.tsv"] += "\n".join(airr.splitlines()[1:])
+        return output
+    needed = " ".join([pair[0] + "=" + str(pair[1]) for pair in supported])
+    observed = " ".join([pair[0] + "=" + str(config.get(pair[0])) for pair in supported])
+    raise NotImplementedError(("Only " + needed + " currently supported, not " + observed))
 
 def load_config(path):
     """Load YAML config file."""
@@ -35,10 +100,52 @@ def layer_configs(*configs):
         config_full.update(config)
     return config_full
 
+
+def _chunker(iterator, chunksize):
+    """Iterate over another iterator in fixed-size chunks.
+
+    This gives a list for each chunk (not another iterator) so it puts each
+    chunk in memory at once.
+    """
+    # inspired by https://stackoverflow.com/a/8991553/4499968
+    chunk = []
+    try:
+        for item in iterator:
+            chunk.append(item)
+            if len(chunk) == chunksize:
+                yield chunk
+                chunk = []
+    except StopIteration:
+        pass
+    yield chunk
+
+def _unzip(txt):
+    """Extract .zip data from bytes into dict keyed on filenames."""
+    with BytesIO(txt) as f_in:
+        zipobj = ZipFile(f_in)
+        zipdata = {}
+        for item in zipobj.infolist():
+            with zipobj.open(item) as stream:
+                zipdata[item.filename] = stream.read()
+    return zipdata
+
+def __load_options():
+    data = load_config(Path(__file__).parent / "data" / "options.yml")
+    mapping = {"int": int, "bool": bool, "str": str}
+    for opt_section in data:
+        for val in opt_section["options"].values():
+            try:
+                val["values"] = mapping.get(val["values"], val["values"])
+            except TypeError:
+                pass
+    return data
+
+def __load_default_config():
+    return load_config(Path(__file__).parent / "data" / "defaults.yml")
+
 def __setup_arg_parser():
     parser = argparse.ArgumentParser(
         description=__doc__,
-        #formatter_class=argparse.RawDescriptionHelpFormatter)
         formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument("config", nargs="*", help="YAML configuration file")
     parser.add_argument(
@@ -60,56 +167,6 @@ def __setup_arg_parser():
                     # https://stackoverflow.com/a/16985727/4499968
             option_parser.add_argument("--" + optname, **args)
     return parser
-
-def vquest_main():
-    """Command-line interface for V-QUEST requests"""
-    parser = __setup_arg_parser()
-    args = parser.parse_args()
-    LOGGER.setLevel(max(10, logging.WARNING - 10*args.verbose))
-    LOGGER.debug("args parsed")
-    args_set = {key: val for key, val in vars(args).items() if val}
-    if not args_set:
-        parser.print_help()
-        sys.exit(1)
-    LOGGER.debug(" ".join(["%s=%s" % (key, val) for key, val in args_set.items()]))
-    configs = [load_config(config) for config in args.config]
-    config_full = layer_configs(DEFAULTS, *configs)
-    vquest(config_full)
-
-def vquest(config):
-    """Submit a request to V-QUEST"""
-    print(config)
-    #vquest_post(config)
-
-def vquest_post(post_data):
-    """Submit a single POST request to V-QUEST"""
-    result = requests.post(URL, data = post_data)
-    print(result)
-
-def vquest_curl(post_data):
-    """Submit a single POST request to V-QUEST via curl"""
-    args = [('-F', '"%s=%s"' % (key, val)) for key, val in post_data.items()]
-    args = [item for sublist in args for item in sublist]
-    args = ["curl"] + args + ["--output", "test.zip", URL]
-
-    result = subprocess.run(args, capture_output=True, check=True)
-    print(result)
-    print(result.stdout)
-    print(result.stderr)
-
-def __load_options():
-    data = load_config(Path(__file__).parent / "data" / "options.yml")
-    mapping = {"int": int, "bool": bool}
-    for opt_section in data:
-        for val in opt_section["options"].values():
-            try:
-                val["values"] = mapping.get(val["values"], val["values"])
-            except TypeError:
-                pass
-    return data
-
-def __load_default_config():
-    return load_config(Path(__file__).parent / "data" / "defaults.yml")
 
 DEFAULTS = __load_default_config()
 OPTIONS = __load_options()
