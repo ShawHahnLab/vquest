e16e9893e569b77b89db64b966cdc02ceea2d1e4
Jesse Connell <ancon@upenn.edu>
2022-09-27 10:36:12 -0400

    make package namespace match readme

--- vquest/__init__.py
+++ vquest/__init__.py
@@ -6,6 +6,8 @@ http://www.imgt.org/IMGT_vquest/analysis
 
 import logging
 from .version import __version__
+from .request import vquest
+from .config import DEFAULTS, OPTIONS, load_config, layer_configs
 LOGGER = logging.getLogger(__name__)
 LOGGER.propagate = False
 LOGGER.addHandler(logging.StreamHandler())
